1f7047c2cfc735d0d6e249f1908482517a2c6a63
dmora219 <dmora14633@gmail.com>
2023-05-31 21:29:37 -0500

    "initial commit"

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function checkDriverAge() {
+   var age = prompt("What is your age?");
+
+   if (Number(age) < 18) {
+      alert("Sorry, you are too yound to drive this car. Powering off");
+   } else if (Number(age) > 18) {
+      alert("Powering On. Enjoy the ride!");
+   } else if (Number(age) === 18) {
+      alert("Congratulations on your first year of driving. Enjoy the ride!");
+   }
+}
+checkDriverAge()
+
+function checkDriverAge2(age) {
+   var age = prompt("What is your age?");
+
+   if (Number(age) < 18) {
+      return "Sorry, you are too yound to drive this car. Powering off";
+   } else if (Number(age) > 18) {
+      return "Powering On. Enjoy the ride!";
+   } else if (Number(age) === 18) {
+      return "Congratulations on your first year of driving. Enjoy the ride!";
+   }
+}
+
+console.log(checkDriverAge2())
